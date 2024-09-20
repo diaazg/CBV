4858066fda46ef42dfcad9ec8827b00b946c7c3e
diaazg <mohamedzellagui585@gmail.com>
2024-09-20 21:11:25 +0100

    video call

--- basic/consumers.py
+++ basic/consumers.py
@@ -216,32 +216,26 @@ class DirectChatConsumer(AsyncWebsocketConsumer):
 
     
     async def handle_start_video_call(self,text_data_json):
-        sender_id = text_data_json['sender_id']
-        receiver_id = text_data_json['receiver_id']
 
-        sender_token = await self.generate_twilio_token(sender_id, self.room_name)
+
+        
         await self.channel_layer.group_send(
         self.room_group_name,
         {
             'type': 'video_call_invite',
-            'sender': sender_id,
-            'receiver': receiver_id,
-            'twilio_token': sender_token,
+
+            
             'room_name': self.room_name
         }
     )
         
     async def video_call_invite(self, event):
-     sender = event['sender']
-     receiver = event['receiver']
-     twilio_token = event['twilio_token']
+
+    
      room_name = event['room_name']
 
      await self.send(text_data=json.dumps({
         'action': 'start_video_call',
-        'sender': sender,
-        'receiver': receiver,
-        'twilio_token': twilio_token,
         'room_name': room_name
     }))        
    
